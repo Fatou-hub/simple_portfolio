17d274d2de3f03f5b9d038212c9431e749457b03
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-03-27 17:49:00 +0100

    update portfolio before hosting

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function scrollToNav(event) {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    const navElement = document.querySelector("nav"); // Sélectionne la première balise <nav>
+  
+    if (navElement) {
+      navElement.scrollIntoView({ behavior: "smooth" }); // Défilement fluide vers la navigation
+    }
+  }
+  
