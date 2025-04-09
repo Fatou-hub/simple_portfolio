62c33504c9f499974df31840780f58cbea26502d
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2025-04-09 07:55:42 +0200

    resume updated

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMenu() {
 
   function scrollToNav(event) {
     event.preventDefault(); // Empêche le comportement par défaut du lien
-    const navElement = document.querySelector("nav"); // Sélectionne la première balise <nav>
+    const navElement = document.querySelector(".container-nav"); // Sélectionne la première balise <nav>
   
     if (navElement) {
       navElement.scrollIntoView({ behavior: "smooth" }); // Défilement fluide vers la navigation
